9272eb92435180a6beb39848e2f2ce7bb6ccc7ca
BUD <SBUD@steelcase.com>
2023-04-28 12:17:47 +0300

    MVP BudHQ

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
